982a7a5a7544dcf256d4127f2148eb1e9c1599a1
Alex Babel <alexander.babel@icloud.com>
2023-03-11 14:08:18 +0100

    ci: update vars

--- .github/scripts/format.sh
+++ .github/scripts/format.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 files=$(for file in "$@"; do echo "$file" | sed 's/\/Dockerfile$//'; done | awk '!a[$0]++' | jq -nR '[inputs | select(length>0)]')
-echo ::set-output name=files::${files}
+echo files=${files} >> "$GITHUB_ENV"
